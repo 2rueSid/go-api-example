1bc1474f6d9d5b37b79472564ea36a70be8c7b67
2rueSid <mekaa2.718@gmail.com>
2021-08-11 18:24:11 +0300

    added get method; rebuild model to use chanels

---
+++ src/types/user.go
@@ -0,0 +1,19 @@
+// package to implement types
+package types
+
+import "github.com/2rueSid/go-api-example/prisma/db"
+
+// DTO that describes which data
+// should pass while createing new user
+type UserInput struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Struct which use to describe CreateUser chanel type
+type UserOutput struct {
+	User      *db.UserModel
+	Err       error
+	ErrStatus int
+}
